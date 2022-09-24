ef485c1ac09e1bc40590bc044f3f36432ed9f148
kdh <sparkle0201@gmail.com>
2022-09-24 22:11:06 +0900

    FEAT : JSON 안에있는 LIST를 한줄로 출력

--- src/main/java/com/jsonparsing/Json/controller/JsonParsingController.java
+++ src/main/java/com/jsonparsing/Json/controller/JsonParsingController.java
@@ -1,16 +1,36 @@
 package com.jsonparsing.Json.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/json")
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/json")
 public class JsonParsingController {
 
     @GetMapping("/parsing")
-    public ResponseEntity<String> parsing(@RequestBody String json){
+    public ResponseEntity parsing(@RequestBody HashMap<String, Object> json){
 
-        return ResponseEntity.ok(json);
+    HashMap result = (HashMap) json.get("raw");
+    ArrayList result2 = (ArrayList) result.get("blocks");
+
+    StringBuilder text = new StringBuilder();
+
+    for (int i =0; i<result2.size();i++){
+        HashMap result3 = (HashMap) result2.get(i);
+        String result4 = (String) result3.get("text");
+        text.append(result4);
+    }
+    return ResponseEntity.ok(text);
     }
 }
